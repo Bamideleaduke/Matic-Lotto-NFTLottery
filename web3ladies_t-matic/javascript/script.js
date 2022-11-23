cce991cd3641bf03806fa99273c265e4494ba63a
Bamideleaduke <bamideleaduke@gmail.com>
2022-11-23 22:37:46 +0100

    play screen

--- web3ladies_t-matic/javascript/script.js
+++ web3ladies_t-matic/javascript/script.js
@@ -1,3 +1,4 @@
+
 // header collapsible
 const toggle = document.querySelector(".menu-bar");
 const close = document.querySelector(".close-bar");
@@ -33,6 +34,48 @@ walletLink.forEach(wallet => {
     })
 });
 
+// play game event
+  const inputBox = document.querySelectorAll(".num-input")
+  const numBtn = document.querySelectorAll(".num");
+  console.log(inputBox);
+ 
+  // select number
+  let number;
+  numBtn.forEach(num => {
+      num.addEventListener("click",(e) => {
+      number =  e.target.textContent
+      console.log(number);
+    })
+  });
+    // get value
+    let value ;
+    inputBox.forEach(ele=> {
+         value = [...ele.innerHTML]
+      // console.log(ele.textContent);
+    });
+    console.log(value);
+// Game section
+const gameCard = document.querySelector(".game-num");
+let firstTouch;
+let lastTouch;
+function swipeDirection(){
+    let touchDiff = firstTouch - lastTouch;
+    if(touchDiff > 100) gameCard.style.marginLeft = "-60%"
+    if(touchDiff < -100) gameCard.style.marginLeft = "0%"
+}
+  
+gameCard.addEventListener("touchstart", (ev) => {
+    firstTouch = ev.touches[0].screenX
+})
+gameCard.addEventListener("touchmove", (ev) => {
+  lastTouch = ev.touches[0].screenX
+  console.log(lastTouch);
+
+  swipeDirection()
+
+})
+// Game section
+
 
 // Hafsah's Slider
 const swiper = new Swiper('.swiper', {
@@ -56,3 +99,7 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+
+
+
