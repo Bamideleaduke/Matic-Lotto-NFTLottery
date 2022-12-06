793a04b051a941fac6a6f3952a6f42720e3cf1f6
Bamideleaduke <bamideleaduke@gmail.com>
2022-12-06 18:57:59 +0100

    claim eth

--- web3ladies_t-matic/javascript/claim-reward.js
+++ web3ladies_t-matic/javascript/claim-reward.js
@@ -1,4 +1,10 @@
 const connect = document.getElementById('connect')
+const claimReward = document.querySelector(".eth-reward")
+claimReward.addEventListener("click",() => {
+    alert(`1Eth has been added to your wallet
+    Thank You
+    `)
+})
 
 function myFunction() {
     document.querySelectorAll('.connect-container')[0].style.display= 'block';
@@ -10,3 +16,4 @@ function nextConnect() {
     document.querySelectorAll('.connect-container')[1].style.display= 'block';
     
 }
+
